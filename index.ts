36b5b5d4775f78ab1e1bbb08bc4e49018125cb3d
Dheyson Alves <dheyson.alves@softmakers.pt>
2020-10-02 21:33:33 -0300

    feat: Add eslint with prettier

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
 /**
  * @format
  */
-
-import {AppRegistry} from 'react-native';
+import {getStorybookUI, configure} from '@storybook/react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {AppRegistry} from 'react-native';
 
 AppRegistry.registerComponent(appName, () => App);
+
+configure(() => {
+  require('./src/stories.js'); // we will create this file in the next steps
+}, module);
+
+const StorybookUIRoot = getStorybookUI({});
+
+AppRegistry.registerComponent(appName, () => StorybookUIRoot);
