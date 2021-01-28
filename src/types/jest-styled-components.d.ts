f607ffa63a2e105dd54b896e87a2224c5d30b619
Dheyson Alves <dheyson.lucas@hotmail.com>
2021-01-27 22:41:42 -0300

    feat: adding new dependencies

---
+++ src/types/jest-styled-components.d.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// eslint-disable-next-line @typescript-eslint/ban-types
+import {Plugin, NewPlugin} from 'pretty-format';
+
+declare global {
+  namespace jest {
+    interface AsymmetricMatcher {
+      $$typeof: symbol;
+      sample?: string | RegExp | object | Array<any> | Function;
+    }
+    type Value = string | number | RegExp | AsymmetricMatcher | undefined;
+
+    interface Options {
+      media?: string;
+      modifier?: string;
+      supports?: string;
+    }
+
+    interface Matchers<R> {
+      toHaveStyleRule(property: string, value?: Value, options?: Options): R;
+    }
+  }
+}
+
+export declare const StyleSheetSerializer: Exclude<Plugin, NewPlugin>;
